be5efce922824de2b8dac1fc5219a709ffed1187
Jhesterquloto <hmtethemhmte@gmail.com>
2024-10-30 17:01:01 +0330

    golestan

---
+++ main.js
@@ -0,0 +1,51 @@
+function fuh(){
+ document.getElementById("id1").style.display="block";
+
+ document.getElementById("id1").style.transition="300ms";
+}
+
+function feh(){
+    var x= document.getElementById("id1").style.display="none";
+
+
+    }
+
+    function fuh2(){
+        document.getElementById("id2").style.display="block";
+       
+        document.getElementById("id2").style.transition="300ms";
+       }
+       
+       function feh2(){
+           var x= document.getElementById("id2").style.display="none";
+       
+       
+           }
+
+
+           function fuh3(){
+            document.getElementById("id3").style.display="block";
+           
+            document.getElementById("id3").style.transition="300ms";
+           }
+           
+           function feh3(){
+               var x= document.getElementById("id3").style.display="none";
+           
+           
+               }
+
+
+
+
+               function fuh4(){
+                document.getElementById("id4").style.display="block";
+               
+                document.getElementById("id4").style.transition="300ms";
+               }
+               
+               function feh4(){
+                   var x= document.getElementById("id4").style.display="none";
+               
+               
+                   }
